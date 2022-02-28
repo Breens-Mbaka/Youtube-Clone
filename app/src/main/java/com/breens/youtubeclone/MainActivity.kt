32d8789d52c3d0515c3ddb15ca1f33e7eb25f00c
user <user@users-MacBook-Air.local>
2022-02-28 21:16:06 +0300

    Created the filter chips

--- app/src/main/java/com/breens/youtubeclone/MainActivity.kt
+++ app/src/main/java/com/breens/youtubeclone/MainActivity.kt
@@ -2,10 +2,45 @@ package com.breens.youtubeclone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.breens.youtubeclone.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        setupNav()
     }
+
+    private fun setupNav() {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        //set nav to automatically assign destinations from menu
+        binding.bottomNavigationView.setupWithNavController(navController)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.fragmentHomeScreen -> showBottomNav()
+                else -> hideBottomNav()
+            }
+        }
+    }
+
+    private fun showBottomNav() {
+        binding.bottomNavigationView.visibility = View.VISIBLE
+    }
+
+    private fun hideBottomNav() {
+        binding.bottomNavigationView.visibility = View.GONE
+
+    }
+
 }
---
+++ app/src/main/java/com/breens/youtubeclone/splashscreen/FragmentSplash.kt
@@ -0,0 +1,44 @@
+package com.breens.youtubeclone.splashscreen
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.breens.youtubeclone.R
+import com.breens.youtubeclone.databinding.FragmentSplashScreenBinding
+
+class FragmentSplash: Fragment(R.layout.fragment_splash_screen) {
+    private var _binding: FragmentSplashScreenBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentSplashScreenBinding.inflate(inflater, container, false)
+        hideActionBar()
+        navigateToHomeScreen()
+        return binding.root
+    }
+
+    private fun navigateToHomeScreen() {
+        Handler(Looper.getMainLooper()).postDelayed({
+            findNavController().navigate(R.id.action_splashScreen_to_fragmentHomeScreen)
+        },3000)
+    }
+
+    private fun hideActionBar() {
+        (activity as AppCompatActivity).supportActionBar?.hide()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
